7a72c601854f3833fdb57c34e0315e4495ff854a
Loic <llh1@sanger.ac.uk>
2013-01-10 14:03:27 +0000

    [#42198671] Handle exception

--- test/start_auditor.rb
+++ test/start_auditor.rb
@@ -6,4 +6,10 @@ audit_settings = YAML.load_file(File.join('config','audit.yml'))["development"]
 auditor = Lims::MessageBusClient::Auditor.new(amqp_settings, audit_settings["audit_file"])
 puts "Auditor starts..."
 puts "Incoming messages are stored in #{audit_settings["audit_file"]}" 
-auditor.start
+
+begin
+  auditor.start
+rescue Exception => e
+  puts "Something went wrong..."
+  puts e.inspect
+end
