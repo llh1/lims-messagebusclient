7341ecc041cdf2a144f9856287e95b2ed3e8ee83
Loic <llh1@sanger.ac.uk>
2013-01-11 12:10:57 +0000

    [#42198671] Update script

--- test/start_auditor.rb
+++ test/start_auditor.rb
@@ -3,7 +3,7 @@ require 'lims-messagebusclient'
 amqp_settings = YAML.load_file(File.join('config','amqp.yml'))["development"] 
 audit_settings = YAML.load_file(File.join('config','audit.yml'))["development"] 
 
-auditor = Lims::MessageBusClient::Auditor.new(amqp_settings, audit_settings["audit_file"])
+auditor = Lims::MessageBusClient::Auditor.new("audit", amqp_settings, audit_settings["audit_file"])
 puts "Auditor starts..."
 puts "Incoming messages are stored in #{audit_settings["audit_file"]}" 
 
