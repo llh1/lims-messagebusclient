1e6312f13f49eb0bca86f5003ceb900012404f55
Loic <llh1@sanger.ac.uk>
2013-01-16 12:58:10 +0000

    [#42460857] Improve plate creator

--- lib/lims-messagebusclient/consumer/sequencescape_plate_creator.rb
+++ lib/lims-messagebusclient/consumer/sequencescape_plate_creator.rb
@@ -58,7 +58,10 @@ module Lims
       # We need messages when a plate is created and when order is
       # created or updated.
       def routing_keys
-        ["*.*.plate.create", "*.*.order.create", "*.*.order.updateorder"]
+        ["*.*.plate.create",
+         "*.*.order.create",
+         "*.*.order.updateorder",
+         "*.*.platetransfer.platetransfer"]
       end
 
       # Setup the queue.
@@ -69,9 +72,12 @@ module Lims
           # On reception of a plate creation message
           if metadata.routing_key =~ /plate\.create/
             plate_message_handler(metadata, payload)       
-            # On reception of an order creation/update message
+          # On reception of an order creation/update message
           elsif metadata.routing_key =~ /order\.create|updateorder/
             order_message_handler(metadata, payload)
+          # On reception of a plate transfer message
+          elsif metadata.routing_key =~ /platetransfer/
+            platetransfer_message_handler(metadata, payload)
           end
         end
       end
@@ -93,14 +99,19 @@ module Lims
 
       # When an order message is received,
       # we check if it contains an item which is a stock plate 
-      # with a done status. Otherwise, we just ignore the message.
-      # We try to update the plate on Sequencescape, if the plate
+      # with a done status. Otherwise, we just ignore the message
+      # and delete the plates which could have been saved in sequencescape
+      # but aren't stock plate.
+      # We try to update the stock plate on Sequencescape, if the plate
       # is not found in Sequencescape, the message is requeued.
       # @param [AMQP::Header] metadata
       # @param [String] order json
       def order_message_handler(metadata, order_json)
         order = JSON.parse(order_json)["order"]  
         stock_plate = order["items"][STOCK_PLATE]
+        other_plates = order["items"].keep_if {|k,v| k != STOCK_PLATE}
+        delete_unassigned_plates_in_sequencescape(other_plates)
+
         if stock_plate && stock_plate["status"] == ITEM_DONE_STATUS 
           begin
             update_plate_purpose_in_sequencescape(stock_plate["uuid"])
@@ -115,6 +126,20 @@ module Lims
         else
           metadata.ack
         end 
+      end
+
+      def platetransfer_message_handler(metadata, platetransfer_json)
+        platetransfer = JSON.parse(platetransfer_json)["plate_transfer"]
+        plate = platetransfer["result"]["plate"]
+
+        begin
+          update_aliquots_in_sequencescape(plate["uuid"], plate["wells"])
+        rescue Sequel::Rollback => e
+          metadata.reject(:requeue => true)
+          puts "Error updating plate aliquots in Sequencescape: #{e}"
+        else
+          metadata.ack
+        end
       end
 
       # Create a plate in Sequencescape database.
@@ -143,7 +168,7 @@ module Lims
           # Save wells
           asset_size = plate["number_of_rows"] * plate["number_of_columns"]
           plate["wells"].each do |well|
-            map_id = t_maps.select(:id).where(:description => well.first, :asset_size => asset_size)
+            map_id = t_maps.select(:id).where(:description => well.first, :asset_size => asset_size).first[:id]
             well_id = t_assets.insert(:sti_type => WELL, :map_id => map_id) 
             t_container_associations.insert(:container_id => plate_id, :content_id => well_id) 
           end
@@ -162,10 +187,60 @@ module Lims
         t_uuids = @db[:uuids]
 
         @db.transaction do
-          plate_id = t_uuids.select(:resource_id).where(:external_id => plate_uuid)                            
+          plate_id = t_uuids.select(:resource_id).where(:external_id => plate_uuid).first[:resource_id]
           raise PlateNotFoundInSequencescape if plate_id.nil?
 
           t_assets.where(:id => plate_id).update(:plate_purpose_id => STOCK_PLATE_PURPOSE_ID) 
+        end
+      end
+
+      # Delete plates and their informations in Sequencescape
+      # database if the plate appears in item order and is not
+      # a stock plate.
+      # @param [Hash] plates
+      def delete_unassigned_plates_in_sequencescape(plates)
+        plates.each do |_, item|
+          t_assets = @db[:assets]
+          t_uuids = @db[:uuids]
+          t_container_associations = @db[:container_associations]
+          plate_uuid = item["uuid"]
+
+          begin
+          @db.transaction do
+            plate = t_assets.select(:assets__id).join(:uuids, :resource_id => :id).where(:external_id => plate_uuid).first
+            unless plate.nil?
+              # Delete wells in assets
+              well_ids = container_associations.select(:assets__id).join(:assets, :id => :content_id).where(:container_id => id).all.inject([]) {|m,e| m << e[:id]}
+              t_assets.where(:id => well_ids).delete
+              # Delete container_associations
+              t_container_associations.where(:content_id => wells_id).delete
+              # Delete plate in assets
+              t_assets.where(:id => plate[:id]).delete
+              # Delete plate uuid
+              t_uuis.where(:external_id => plate_uuid).delete
+            end
+          end
+          rescue Sequel::Rollback => e
+          end
+        end
+      end
+
+
+
+      def update_aliquots_in_sequencescape(plate_uuid, s2_wells)
+        t_assets = @db[:assets]
+        t_aliquots = @db[:aliquots]
+        t_samples = @db[:samples]
+
+        s2_wells.each do |location, aliquots|
+          unless aliquots.empty?
+            plate_id = t_assets.select(:id).join(:uuids, :external_id => :id).where(:id => plate_uuid)
+
+            aliquots.each do |aliquot|
+#              t_assets.select(:assets.*).join(:uuids, :resource_id => :id).where(:external_id => uuid).first[:id]
+
+            end
+          end
         end
       end
     end
